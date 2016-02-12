4822eee359dc0b10f4cf26ce6c21386a186b51fb
Илья Хованский <saskiuhia@gmail.com>
2016-02-12 15:27:43 +0200

    Make simple UI

--- NumericalMethods/NumericalMethods/MainWindow.xaml.cs
+++ NumericalMethods/NumericalMethods/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,39 @@ namespace NumericalMethods
         {
             InitializeComponent();
         }
+
+        private void MenuItem_open_file_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open_file = new OpenFileDialog();
+            open_file.DefaultExt = Directory.GetCurrentDirectory();
+            open_file.ShowDialog();
+        }
+        private void MenuItem_save_to_file_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save_file = new SaveFileDialog();
+            save_file.DefaultExt = Directory.GetCurrentDirectory();
+            save_file.ShowDialog();
+        }
+
+        private void MenuItem_gauss_with_main_element_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem_calculate.IsEnabled = true;
+            MenuItem_gauss_seidel.IsChecked = false;
+        }
+        private void MenuItem_gauss_seidel_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem_calculate.IsEnabled = true;
+            MenuItem_gauss_with_main_element.IsChecked = false;
+        }
+
+        private void MenuItem_calculate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_help_Click(object sender, RoutedEventArgs e)
+        {
+
+        }        
     }
 }
