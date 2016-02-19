ec5e1720aed97ef7e6367705d29236cfdc35fc66
Илья Хованский <saskiuhia@gmail.com>
2016-02-19 05:07:18 +0200

    Make work with file. Added more features in interface

--- NumericalMethods/NumericalMethods/MainWindow.xaml.cs
+++ NumericalMethods/NumericalMethods/MainWindow.xaml.cs
@@ -22,91 +22,156 @@ namespace NumericalMethods
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<object> matrix_values; 
+        private List<double[]> matrix_values;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            matrix_values = new List<object>();         
-
-    }
-
-    private void MenuItem_open_file_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog open_file = new OpenFileDialog();
-            open_file.DefaultExt = Directory.GetCurrentDirectory();
-            open_file.ShowDialog();
+            matrix_values = new List<double[]>();
+            open_file = new OpenFileDialog();
+            save_file = new SaveFileDialog();
         }
+
+        OpenFileDialog open_file;
+        private void MenuItem_open_file_Click(object sender, RoutedEventArgs e)
+        {
+            open_file.FileOk += Open_file_FileOk;
+            open_file.InitialDirectory = Directory.GetCurrentDirectory();
+            open_file.Multiselect = false;
+            open_file.Filter = "Text Files | *.txt";
+            open_file.FileName = "matrix";
+            open_file.ShowDialog();
+
+        }
+        private void Open_file_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                StreamReader file_reader = new StreamReader(File.Open(open_file.FileName, FileMode.Open));
+                int size = int.Parse(file_reader.ReadLine());
+                double[][] matrix = new double[size][];
+                for (int i = 0; i < size; i++)
+                {
+                    string row = file_reader.ReadLine();
+                    List<string> values = row.Split(new char[] { ' ' }).ToList();
+                    if (values.Last() == "")
+                        values.Remove(values.Last());
+
+                    if (values.Count != size + 1)
+                        throw new ArgumentNullException();
+
+                    matrix[i] = new double[size + 1];
+
+                    for (int j = 0; j < size + 1; j++)
+                        matrix[i][j] = double.Parse(values[j]);
+                }
+
+                matrix_values.Clear();
+                Generate_dataGrid(size);
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size + 1; j++)
+                        matrix_values[i][j] = matrix[i][j];
+                }
+                dataGrid_matrix.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Невозможно открыть файл!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        SaveFileDialog save_file;
         private void MenuItem_save_to_file_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog save_file = new SaveFileDialog();
-            save_file.DefaultExt = Directory.GetCurrentDirectory();
+            if (matrix_values.Count == 0)
+            {
+                MessageBox.Show("Матрица значений не заполнена!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            save_file.FileOk += Save_file_FileOk;
+            save_file.InitialDirectory = Directory.GetCurrentDirectory();
+            save_file.Filter = "Text Files | *.txt";
+            save_file.FileName = "matrix";
             save_file.ShowDialog();
+        }
+        private void Save_file_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                StreamWriter file_writer = new StreamWriter(File.Open(save_file.FileName, FileMode.Create));
+                double[][] matrix = Get_matrix_from_dataGrid();
+                file_writer.WriteLine(matrix.Length);
+
+                foreach (var row in matrix_values)
+                {
+                    foreach (var value in row)
+                    {
+                        file_writer.Write(value);
+                        file_writer.Write(" ");
+                    }
+
+                    file_writer.Write(file_writer.NewLine);
+                }
+                file_writer.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Невозможно сохранить файл!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
         }
 
         private void MenuItem_gauss_with_main_element_Click(object sender, RoutedEventArgs e)
         {
-            MenuItem_calculate.IsEnabled = true;
             MenuItem_gauss_with_main_element.IsChecked = true;
             MenuItem_gauss_seidel.IsChecked = false;
-
-            textBox_result.Clear();
         }
         private void MenuItem_gauss_seidel_Click(object sender, RoutedEventArgs e)
         {
-            MenuItem_calculate.IsEnabled = true;
             MenuItem_gauss_with_main_element.IsChecked = false;
             MenuItem_gauss_seidel.IsChecked = true;
-
-            textBox_result.Clear();
         }
 
         private void MenuItem_calculate_Click(object sender, RoutedEventArgs e)
         {
-            textBox_result.Clear();
+            if (matrix_values.Count == 0)
+            {
+                MessageBox.Show("Матрица значений не заполнена!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (MenuItem_gauss_seidel.IsChecked == false && MenuItem_gauss_with_main_element.IsChecked == false)
+            {
+                MessageBox.Show("Не выбран алгоритм решения!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            double[] roots = null;
+            double[][] matrix = Get_matrix_from_dataGrid().ToArray();
 
             if (MenuItem_gauss_with_main_element.IsChecked == true)
             {
-                double[,] myArr = new double[3, 4];
-
-                Random ran = new Random();
-
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        myArr[i, j] = ran.Next(0, 9);
-                    }
-                }
-
-                double[] x = new double[3];          
-
-                if (Methods.Gauss_with_main_element(myArr, 3)==null)
-                {
-                    textBox_result.Text = "Данная система не имеет решений!";
-                }
-                else
-                {
-                    x = Methods.Gauss_with_main_element(myArr, 3);
-
-                    foreach (double d in x)
-                    {
-                        textBox_result.Text += d.ToString() + ";";
-                    }
-                }
-                 
-
-                
-
-
-
+                roots = Methods.Gauss_with_main_element(matrix);
             }
 
             if (MenuItem_gauss_seidel.IsChecked == true)
             {
-                Methods.Gauss_seidel();
+                double accuracy = double.NaN;
+                if (double.TryParse(textBox_accuracy.Text, out accuracy))
+                {
+                    MessageBox.Show("Некорректно задана точность!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                roots = Methods.Gauss_seidel(Get_matrix_from_dataGrid(), accuracy);
             }
+
+            Show_roots(roots);
+
         }
 
         private void MenuItem_help_Click(object sender, RoutedEventArgs e)
@@ -119,8 +184,16 @@ namespace NumericalMethods
             dataGrid_matrix.Columns.Clear();
             matrix_values.Clear();
 
-            int size = int.Parse(textBox_size.Text);
-
+            int size = -1;
+            if(int.TryParse(textBox_size.Text, out size) == false)
+            {
+                MessageBox.Show("Некорректно задан размер генерируемой матрицы!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }            
+            Generate_dataGrid(size);
+        }
+        private void Generate_dataGrid(int size)
+        {
             DataGridTextColumn column;
             double[] row;
             for (int i = 1; i <= size; i++)
@@ -133,6 +206,7 @@ namespace NumericalMethods
                 row = new double[size + 1];
                 for (int j = 0; j < size; j++)
                     row[j] = 0;
+
                 matrix_values.Add(row);
             }
 
@@ -144,10 +218,63 @@ namespace NumericalMethods
             dataGrid_matrix.ItemsSource = matrix_values;
             dataGrid_matrix.Items.Refresh();
 
-            while(size +1 != dataGrid_matrix.Columns.Count)
+            while (size + 1 != dataGrid_matrix.Columns.Count)
             {
                 dataGrid_matrix.Columns.RemoveAt(dataGrid_matrix.Columns.Count - 1);
             }
+        }
+
+        private void button_random_matrix_Click(object sender, RoutedEventArgs e)
+        {
+            if (matrix_values.Count == 0)
+            {
+                MessageBox.Show("Матрица значений не сгенерирована!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            double accuracy = double.NaN;
+            if(double.TryParse(textBox_accuracy.Text, out accuracy))
+            {
+                MessageBox.Show("Некорректно задана точность!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            int accuracy_count = 0;
+            for (; accuracy != Math.Round(accuracy); accuracy_count++)
+                accuracy *= 10;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < matrix_values.Count; i++)
+            {
+                for (int j = 0; j < matrix_values[i].Length; j++)
+                    matrix_values[i][j] = Math.Round(rand.NextDouble() * rand.Next(-10, 10), accuracy_count);
+            }
+
+            dataGrid_matrix.Items.Refresh();
+        }
+
+        private double[][] Get_matrix_from_dataGrid()
+        {
+            if (matrix_values.Count == 0)
+                return null;
+
+            return matrix_values.ToArray();
+        }
+
+        private void Show_roots(double[] roots)
+        {
+            if (roots == null || roots.Length == 0)
+            {
+                MessageBox.Show("Невозможно получить корни!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string message = "";
+
+            for (int i = 0; i < roots.Length; i++)
+                message += "X" + (i + 1) + " = " + roots[i] + (i == roots.Length - 1 ? "" : "\n");
+
+            MessageBox.Show(message, "Корни", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
--- NumericalMethods/NumericalMethods/Methods.cs
+++ NumericalMethods/NumericalMethods/Methods.cs
@@ -9,76 +9,90 @@ namespace NumericalMethods
 {
     public abstract class Methods
     {
-        public static double[] Gauss_with_main_element(double[,] matrix, int size)
+        public static double[] Gauss_with_main_element(double[][] matrix)
         {
-            bool has_solve = true;
-
-            double[] roots = new double[size];
+            int size = matrix.Length;
 
             double main = 0;
-
-            int m = 0;int i = 0;int j = 0;
-
+            int m = 0;
+            int i = 0;
+            int j = 0;
             double bb = 0;
 
-            for (int k = 0; k < size; k++) //searching of max element absolute value in the first column
+            //Поиск максимального элемента в первом столбце
+            for (int k = 0; k < size; k++)
             {
-                main = Math.Abs(matrix[k,k]);
+                main = Math.Abs(matrix[k][k]);
                 i = k;
+
                 for (m = k + 1; m < size; m++)
-                    if (Math.Abs(matrix[m,k]) > main)
+                {
+                    if (Math.Abs(matrix[m][k]) > main)
                     {
                         i = m;
-                        main = Math.Abs(matrix[m,k]);
+                        main = Math.Abs(matrix[m][k]);
                     }
-
-                if (main == 0)  //does the system has solves
-                {
-                    has_solve = false;
                 }
 
-                if (i != k)  //  перестановка i-ой строки, содержащей главный элемент k-ой строки
+                //Проверка на совместность
+                if (main == 0)
+                    return null;
+
+                //Перестановка i-ой строки, содержащей главный элемент k-ой строки
+                if (i != k)
                 {
                     for (j = k; j < size + 1; j++)
                     {
-                        bb = matrix[k,j];
-                        matrix[k,j] = matrix[i,j];
-                        matrix[i,j] = bb;
+                        bb = matrix[k][j];
+                        matrix[k][j] = matrix[i][j];
+                        matrix[i][j] = bb;
                     }
                 }
-                main = matrix[k,k];//преобразование k-ой строки
-                matrix[k,k] = 1;
+
+                //Преобразование k-ой строки
+                main = matrix[k][k];
+                matrix[k][k] = 1;
 
                 for (j = k + 1; j < size + 1; j++)
-                    matrix[k,j] = matrix[k,j] / main;
+                    matrix[k][j] = matrix[k][j] / main;
 
-                for (i = k + 1; i < size; i++)//преобразование строк с помощью k-ой строки
+                //Преобразование строк с помощью k-ой строки
+                for (i = k + 1; i < size; i++)
                 {
-                    bb = matrix[i,k];
-                    matrix[i,k] = 0;
-                    if (bb != 0)
-                        for (j = k + 1; j < size + 1; j++)
-                            matrix[i,j] = matrix[i,j] - bb * matrix[k,j];
-                }
-            }        
+                    bb = matrix[i][k];
+                    matrix[i][k] = 0;
 
-            for (i = size-1; i >= 0; i--)   //Search roots
+                    if (bb != 0)
+                    {
+                        for (j = k + 1; j < size + 1; j++)
+                            matrix[i][j] = matrix[i][j] - bb * matrix[k][j];
+                    }
+                }
+            }
+
+            //Поиск корней
+            double[] roots = new double[size];
+            for (i = size - 1; i >= 0; i--)
             {
                 roots[i] = 0;
-                main = matrix[i,size];
-                for (j = size-1; j > i; j--)
-                    main = main - matrix[i,j] * roots[j];
+                main = matrix[i][size];
+
+                for (j = size - 1; j > i; j--)
+                    main = main - matrix[i][j] * roots[j];
+
                 roots[i] = main;
             }
 
-            if (has_solve == true)
-                return roots; //roots
-            else return null; //system hasn`t got any solves
+            return roots;
         }
 
-        public static void Gauss_seidel()
+        public static double[] Gauss_seidel(double[][] matrix, double accuracy)
         {
+            int size = matrix.Length;
 
+            double[] roots = new double[matrix.Length];
+
+            return roots;
         }
     }
 }
